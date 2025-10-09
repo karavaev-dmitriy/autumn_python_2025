1d422bba45c1e69102d1e41866a99229a1162b60
Sid <kdv100@bk.ru>
2025-10-09 12:21:26 +0300

    lesson4

---
+++ task16.py
@@ -0,0 +1,87 @@
+# todo: База данных пользователя.
+# Задан массив объектов пользователя
+
+users = [{'login': 'Piter', 'age': 23, 'group': "admin"},
+         {'login': 'Ivan',  'age': 10, 'group': "guest"},
+         {'login': 'Dasha', 'age': 30, 'group': "master"},
+         {'login': 'Fedor', 'age': 13, 'group': "guest"}]
+
+# Написать фильтр который будет выводить отсортированные объекты по возрасту(больше введеного)
+# ,первой букве логина, и заданной группе.
+
+#Сперва вводится тип сортировки:
+# 1. По возрасту
+# 2. По первой букве
+# 3. По группе
+#
+# тип сортировки: 1
+
+#Затем сообщение для ввода
+# Ввидите критерии поиска: 16
+
+# Результат:
+#Пользователь: 'Piter' возраст 23 года , группа  "admin"
+#Пользователь: 'Dasha' возраст 30 лет , группа  "master"
+
+
+
+first_letter = [x2 for x2 in users if x2['group'].lower() == 'guest'.lower() ]
+for x2 in first_letter: print(x2)
+
+
+print('''
+ Сортировка:
+    1. По возрасту
+    2. По первой букве
+    3. По группе
+    ''')
+
+n  = input('выберите пункт меню: ')
+
+
+match n:
+    case '1':
+        age = int(input('введите возраст: '))
+
+        age_lst = [x for x in users if x['age'] > age]
+        for x in age_lst: print(x)
+
+    case '2':
+        first = input('введите первую букву: ')
+
+        first_letter = [x1 for x1 in users if x1['login'][0].lower() == first.lower()]
+        for x1 in first_letter: print(x1)
+
+    case '3':
+        group = input('введите группу: ')
+
+        group_lst = [x2 for x2 in users if x2['group'].lower() == group.lower()]
+        for x2 in group_lst: print(x2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task18.py
@@ -0,0 +1,24 @@
+#todo: Заданы множества
+#Даны читатели книг
+readers_books = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1' }
+
+#Даны читатели газет
+readers_magazines = { 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+
+# Найти пользователей кто читает и книги и газеты
+
+res = readers_books & readers_magazines
+
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
